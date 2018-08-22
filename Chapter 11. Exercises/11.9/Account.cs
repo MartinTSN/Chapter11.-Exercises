f188d393646d0e682ade4dcb92c71277862b4705
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-08-22 09:57:57 +0200

    Stuff

--- Chapter 11. Exercises/11.9/Account.cs
+++ Chapter 11. Exercises/11.9/Account.cs
@@ -2,5 +2,44 @@
 
 public class Account
 {
+    private decimal accountBalance;
 
+    public Account(decimal accountBalance)
+    {
+        AccountBalance = accountBalance;
+    }
+
+    public decimal AccountBalance
+    {
+        get
+        {
+            return accountBalance;
+        }
+        set
+        {
+            if (value < 0.0m)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), value, "must be greater or equal to 0.0m");
+            }
+            accountBalance = value;
+        }
+    }
+
+    public void AddMoney(decimal money)
+    {
+        AccountBalance = AccountBalance + money;
+    }
+
+    public string Withdrawl(decimal money)
+    {
+        if (money > AccountBalance)
+        {
+            return "Debit amount exceeded account balance.";
+        }
+        else
+        {
+            AccountBalance = AccountBalance - money;
+            return $"{money:C} withdrawed from account";
+        }
+    }
 }
--- Chapter 11. Exercises/11.9/SavingsAccount.cs
+++ Chapter 11. Exercises/11.9/SavingsAccount.cs
@@ -2,5 +2,18 @@
 
 public class SavingsAccount : Account
 {
+    private decimal interestRate;
 
+    public SavingsAccount(decimal interestRate)
+        :base(accountBalance)
+        
+    {
+
+    }
+
+
+    public decimal CalculateInterest(decimal money)
+    {
+        return interestRate * AccountBalance;
+    }
 }
